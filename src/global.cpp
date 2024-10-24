dbfe2308d87e82fabea08250c0dee51d644f6d1f
sakai419 <sakaitaiki.soccer@icloud.com>
2024-10-24 16:23:49 +0900

    Modify global.cpp

--- src/global.cpp
+++ src/global.cpp
@@ -3,7 +3,6 @@
 #include <opencv2/core/core.hpp>
 
 int g_shape = 0;
-long g_difficulty = 1000000;
 double g_highlight = 0.5;
 double g_angle1 = 0.0;
 double g_angle2 = 0.0;
@@ -13,6 +12,7 @@ bool g_isLeftButtonOn = false;
 bool g_isRightButtonOn = false;
 bool g_isCorrect = false;
 State g_state = STATE_HOME;
+Difficulty g_difficulty = EASY;
 auto g_time = high_resolution_clock::now();
 GLfloat light_position[3] = {0.0, 0.0, 0.0};
 GLfloat object_position[3] = {0.0, 0.0, 0.0};
